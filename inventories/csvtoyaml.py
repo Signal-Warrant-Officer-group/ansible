0fb432fe558b91d7dbbf7d00bc8289eb951b055d
Jonathan Marcano <jonathan.marcano@gmail.com>
2023-01-05 23:03:59 -1000

    refactored code to make it cleaner, and added argparser for input/output file arguments

--- inventories/csvtoyaml.py
+++ inventories/csvtoyaml.py
@@ -1,36 +1,21 @@
+import argparse
+import csv
+import os
 import yaml
 from datetime import datetime
-import os
-import csv
-import sys
 
-root = os.getcwd()
-
-# takes a csvFile name and output file name/path
-def csv_to_yaml(csvFile, output):
-    with open(output, 'w', encoding='utf-8') as s:
-        reader = csv.reader(csvFile, delimiter=',')
-        keys = next(reader)
-        keys.append('account_expiration')
-        for row in reader:
-            account_expires = f"{row[3]}/{row[4]}/{row[5]}"
-            dt = datetime.strptime(account_expires, '%m/%d/%Y').date()
-            row.append(dt)
-            yaml.dump([dict(zip(keys, row))], s, sort_keys=False,default_flow_style=False, allow_unicode=True)
-
-
-
-def input_csv_file(csvFile, output=None):
-    output = output if output else root+'/'+(csvFile.split(',')[-1].replace('.csv','.yml'))
-
-    try:
-        with open(csvFile, 'r', encoding='utf-8') as csvFile:
-            csv_to_yaml(csvFile, output)
-    except FileNotFoundError:
-        msg = "No such file or directory: "+ csvFile
-        print(msg)
-
+def csv_to_yaml(csv_file, yaml_file):
+    with open(csv_file, 'r', encoding='utf-8') as csv_f, open(yaml_file, 'w', encoding='utf-8') as yaml_f:
+        reader = csv.DictReader(csv_f, delimiter=',')
+        data = [{**row, 'account_expiration': datetime.strptime(f"{row['month']}/{row['day']}/{row['year']}", '%m/%d/%Y').date()} for row in reader]
+        yaml.dump({'users': data}, yaml_f, sort_keys=False, default_flow_style=False, allow_unicode=True)
+        
+def main(csv_file, yaml_file):
+    csv_to_yaml(csv_file, yaml_file)
 
 if __name__ == '__main__':
-    csvFile = sys.argv[1]
-    input_csv_file(csvFile)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csv_file', help='the input CSV file')
+    parser.add_argument('yaml_file', help='the output YAML file')
+    args = parser.parse_args()
+    main(args.csv_file, args.yaml_file)
