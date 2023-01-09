26143cdd222251017277351696866cdf95b7fd1a
Jonathan Marcano <jonathan.marcano@gmail.com>
2023-01-08 22:52:27 -1000

    Added a few comments to code

--- inventories/csvtoyaml.py
+++ inventories/csvtoyaml.py
@@ -4,9 +4,17 @@ import os
 import yaml
 from datetime import datetime
 
+
 def csv_to_yaml(csv_file, yaml_file):
+    # Open the input CSV file and the output YAML file
     with open(csv_file, 'r', encoding='utf-8') as csv_f, open(yaml_file, 'w', encoding='utf-8') as yaml_f:
+        # Read the CSV file using the csv module's DictReader allows calling column by name.
         reader = csv.DictReader(csv_f, delimiter=',')
+        '''
+        Convert the rows of the CSV file into a list of dictionaries, adding a new key called 
+        'account_expiration' to each dictionary with value for this key being the 'month', 'day', 
+        and 'year' fields.
+        '''
         data = [{**row, 'account_expiration': datetime.strptime(f"{row['month']}/{row['day']}/{row['year']}", '%m/%d/%Y').date()} for row in reader]
         yaml.dump({'users': data}, yaml_f, sort_keys=False, default_flow_style=False, allow_unicode=True)
         
